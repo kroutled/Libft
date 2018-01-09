6d70d26413c7aa42f39c1e7ed0abbe5615fe89a0
Kyle ROUTLEDGE <kroutled@e4r6p9.wethinkcode.co.za>
2018-01-09 15:35:15 +0200

    new libft repo

---
+++ ft_strsub.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kroutled <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/09 08:13:43 by kroutled          #+#    #+#             */
+/*   Updated: 2017/07/26 15:40:11 by kroutled         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(const char *s, unsigned int start, size_t len)
+{
+	char	*mem;
+	size_t	i;
+
+	mem = (char*)malloc(sizeof(char) * len + 1);
+	if (mem == NULL || !s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (NULL);
+	i = 0;
+	while (i < len && s[start] != '\0')
+	{
+		mem[i] = s[start];
+		start++;
+		i++;
+	}
+	mem[i] = '\0';
+	return (mem);
+}
